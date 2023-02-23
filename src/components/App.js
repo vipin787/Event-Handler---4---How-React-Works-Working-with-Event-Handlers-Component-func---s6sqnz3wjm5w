00a785b4273b172e25dbdd9db75322b8c5371de9
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-02-23 09:15:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 const App = () => {
+  let numClicks = 0;
+  let singleClickTimer;
+  const handleClick = () => {
+    numClicks++;
+    if (numClicks === 1) {
+      singleClickTimer = setTimeout(() => {
+        numClicks = 0;
+        console.log("I was not double clicked");
+      }, 400);
+    } else if (numClicks === 2) {
+      clearTimeout(singleClickTimer);
+      numClicks = 0;
+      console.log("I was double clicked");
+    }
+  };
 
-  const handleDoubleClick = (event) =>{
-  
-  }
   return (
     <div id="main">
-      <button id="dblclick-btn">Double click me</button>
+      <button
+        id="dblclick-btn"
+        onClick={(event) => {
+          handleClick(event);
+        }}
+      >
+        Double click me
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
