eb8cdc53a8bebf0a1807f4d3d0dc3a693f1f4636
Vipin Chauhan <110002254+vipin787@users.noreply.github.com>
2023-02-23 09:08:45 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+
+  const handleDoubleClick = (event) =>{
+  
+  }
+  return (
+    <div id="main">
+      <button id="dblclick-btn">Double click me</button>
+    </div>
+  )
+}
+
+
+export default App;
